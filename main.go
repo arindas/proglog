ffd55eb76624c11b7ce789e1a285638ad4ed166c
Arindam Das <dasarindam.mails@gmail.com>
2022-03-10 00:02:08 +0530

    Added grpc defs for log, removed old server module.

--- main.go
+++ main.go
@@ -1,12 +1,4 @@
 package main
 
-import (
-	"log"
-
-	"github.com/arindas/proglog/internal/server"
-)
-
 func main() {
-	server := server.NewHttpServer(":8080")
-	log.Fatal(server.ListenAndServe())
 }
